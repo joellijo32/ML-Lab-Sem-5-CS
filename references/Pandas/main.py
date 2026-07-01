b753e81a11cadff0b51e7a8f59b556acbcd6943f
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-01 23:55:30 +0530

    dataframes adding new rows by concatenating two dictionaries

--- references/Pandas/main.py
+++ references/Pandas/main.py
@@ -8,4 +8,37 @@ data = {
 df = pd.DataFrame(data, index=['guy 1', 'guy 2', 'guy 3'])
 
 df['Major'] = [True, False, False]
+
+new_row = pd.DataFrame(
+    {
+        'Name' : ['Moncy'], 
+        'Age' : 54,
+        'Major' : True
+    }, index=['guy 4']
+)
+
+df  = pd.concat([df, new_row])
+
+# extra rows:
+
+extra_rows = pd.DataFrame([
+        {
+            'Name' : 'Lijo',
+            'Age' : 45,
+            'Major' : True
+        }, 
+        {
+            'Name' : 'Mathew',
+            'Age' : 34,
+            'Major' : True
+        }, 
+        {
+            'Name' : 'Mariyamma' ,
+            'Age' : 56,
+            'Major' : True
+        }
+], index=['guy 5', 'guy 6', 'guy 7']
+)
+
+df = pd.concat([df, extra_rows])
 print(df)
