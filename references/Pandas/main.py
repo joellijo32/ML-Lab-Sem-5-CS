20628b7ceaf5b1c2246f9f101f431add23fac841
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-01 23:20:26 +0530

    Pandas using dictionary

--- references/Pandas/main.py
+++ references/Pandas/main.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
-data = [234, 45,134, 5,54,76]
+calories = {
+    'Day 1' : 45355,
+    'Day 2' : 23413,
+    'Day 3' : 564,
+    'Day 4' : 45426
+}
 
-series = pd.Series(data, index=['a', 'b', 'c', 'd', 'e', 'f'])
-
-print(series[series >= 100])
+series = pd.Series(calories)
+print(series['Day 3'])
