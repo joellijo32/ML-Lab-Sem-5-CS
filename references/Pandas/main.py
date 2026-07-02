d18122c727828274ebc2145d14a39ac2acf8b55b
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-02 18:11:57 +0530

    column selection csv

--- references/Pandas/main.py
+++ references/Pandas/main.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
-df = pd.read_json("references/Pandas/sample.json")
+df = pd.read_csv("references/Pandas/sample-data.csv")
 print(df)
+
+# Selection by column
+
+print(df['Name'].to_string())
