ad0775ac804bee35c000ee829f712216e527489f
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-02 18:55:13 +0530

    Name search using try except block

--- references/Pandas/main.py
+++ references/Pandas/main.py
@@ -6,4 +6,9 @@ df = pd.read_csv("references/Pandas/sample-data.csv", index_col='Name')
 # Selection by column
 
 # print(df.loc["Bulbasaur":'Blastoise', [ "Height", "Weight"]])
-print(df.iloc[0:32:2, 0:3])
+name = input("Enter pokemon name: ")
+
+try:
+    print(df.loc[name])
+except KeyError:
+    print(f"{name} not found")
