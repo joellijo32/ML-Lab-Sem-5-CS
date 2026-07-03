feec14ae96d30f06dfcceefb4672ab66d6bfe504
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-03 17:13:22 +0530

    Multidimensional array in numpy

--- references/Numpy/main.py
+++ references/Numpy/main.py
@@ -1,7 +1,10 @@
 import numpy as np
 
-array = np.array([3,5,78,2])
-print("Array: " , array)
+array = np.array([
+    [['a', 'g', 'f'], ['r', 't', 'e'], ['b', 'n', 'q']],
+    [['a', 'g', 'f'], ['r', 't', 'e'], ['b', 'n', 'q']],
+    [['a', 'g', 'f'], ['r', 't', 'e'], ['b', 'n', 'q']]
+    ])
 
-array = array*2;
-print("Array multiplied by 2: ", array)
+print(array.ndim) # ndim: no. of dimensions
+print(array.shape) # shape: shape of the array (here: 3 x 3 x 3)
