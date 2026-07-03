1d6ecb775a4f07da922b5b6aa3d1d47aad1717bb
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-03 18:31:16 +0530

    Broadcasting: multiple dimensions array operations

--- references/Numpy/main.py
+++ references/Numpy/main.py
@@ -1,50 +1,43 @@
 import numpy as np
 
-# Scalar arithmetic
+# Broadcasting (different dimensions array operations)
+# Either the two corresponding must be the same, or one of them must be one
 
-array = np.array([1, 2, 3])
+array1 = np.array([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12], 
+    [13, 14, 15, 16]
+])
 
-print("Array + 3:", array+3)
+array2 = np.array([
+    [1], 
+    [2], 
+    [3], 
+    [4]
+])
 
-print("Array / 2:", array/2)
+print(array1.shape)
+print(array2.shape)
+print("Multiplication:\n", array1*array2)
 
-print("Array ^ 5:", array**5)
+# Multiplication table:
 
-# Vectorized math functions
+array1 = np.array([
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+])
 
-array = np.array([1.2, 4.6, 6.1])
+array2 = np.array([
+    [1],
+    [2],
+    [3],
+    [4],
+    [5],
+    [6],
+    [7],
+    [8],
+    [9],
+    [10]
+])
 
-print("Square root:", np.sqrt(array))
-
-print("Rounding:", np.round(array))
-
-print("Round down:", np.floor(array))
-
-print("Round up:", np.ceil(array))
-
-print("Pi:", np.pi)
-
-radii = np.array([1, 2, 3])
-
-print("Area:", (np.pi * (radii**2)))
-
-# Element-wise arithmetic
-
-array1 = np.array([1, 2, 3])
-array2 = np.array([4, 5, 6])
-
-print("Sum:", array2 + array1)
-
-print("Multiplication:", array1*array2)
-
-print("Division:", array1/array2)
-
-# Comparison operators
-
-scores = np.array([91, 33, 63, 100, 13, 71])
-
-print("Full marks:", scores==100)
-
-scores[scores<60] = 0
-
-print("Fail:", scores)
+print("\nMultiplication Table:\n", array1*array2)
