50012cafc67902b6149c80b8c4da007cd237683c
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-03 17:57:46 +0530

    slicing arrays

--- references/Numpy/main.py
+++ references/Numpy/main.py
@@ -1,10 +1,26 @@
 import numpy as np
 
 array = np.array([
-    [['a', 'g', 'f'], ['r', 't', 'e'], ['b', 'n', 'q']],
-    [['a', 'g', 'f'], ['r', 't', 'e'], ['b', 'n', 'q']],
-    [['a', 'g', 'f'], ['r', 't', 'e'], ['b', 'n', 'q']]
-    ])
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12], 
+    [13, 14, 15, 16]
+])
 
-word = array[0,1,0] + array[1,0,0] + array[2,1,1]
-print("Word:" , word)
+# array[start: end: step]
+
+print("First row:", array[0:1])
+
+print("\nStep 2:\n", array[::2])
+
+print("\nReverse\n", array[::-1])
+
+print("\nReverse Step 2\n", array[::-2])
+
+print("\nFirst Column\n", array[:,0])
+
+print("\nStep 2 Column\n", array[:, ::2])
+
+print("\nReverse Column\n", array[:, ::-1])
+
+print("\nFirst two rows and columns\n", array[:2, :2])
