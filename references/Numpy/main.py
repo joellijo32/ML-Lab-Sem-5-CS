48fb61aea4def5f3083688e30745605bc7320189
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-03 18:44:46 +0530

    Aggregate functions numpy

--- references/Numpy/main.py
+++ references/Numpy/main.py
@@ -1,43 +1,28 @@
 import numpy as np
 
-# Broadcasting (different dimensions array operations)
-# Either the two corresponding must be the same, or one of them must be one
+# Aggregate Function
 
-array1 = np.array([
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10, 11, 12], 
-    [13, 14, 15, 16]
+array = np.array([
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10]
 ])
 
-array2 = np.array([
-    [1], 
-    [2], 
-    [3], 
-    [4]
-])
+print("Sum:", np.sum(array))
 
-print(array1.shape)
-print(array2.shape)
-print("Multiplication:\n", array1*array2)
+print("Mean:", np.mean(array))
 
-# Multiplication table:
+print("Standard Deviation:", np.std(array))
 
-array1 = np.array([
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-])
+print("Variance:", np.var(array))
 
-array2 = np.array([
-    [1],
-    [2],
-    [3],
-    [4],
-    [5],
-    [6],
-    [7],
-    [8],
-    [9],
-    [10]
-])
+print("Mininum:", np.min(array))
 
-print("\nMultiplication Table:\n", array1*array2)
+print("Maximum:", np.max(array))
+
+print("Position of min:", np.argmin(array))
+
+print("Position of max:", np.argmax(array))
+
+print("Sum of columns:", np.sum(array, axis=0))
+
+print("Sum of rows:", np.sum(array, axis=1))
