437df74961a03612d98d1a80d3ccdcb4b6d3ef2d
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-05 16:13:35 +0530

    Scatter graphs

--- references/Matplotlib/main.py
+++ references/Matplotlib/main.py
@@ -3,17 +3,35 @@ import numpy as np
 
 plt.style.use('dark_background')
 
-categories = np.array(['Freshman', 'Sophomores', 'Juniors', 'Seniors'])
-values = np.array([300, 250, 275, 225])
+x1 = np.array([0, 1, 1, 2, 3, 4, 5, 6, 7, 7, 8])
+y1 = np.array([55, 60, 65, 62, 68, 70, 75, 78, 82, 85, 87])
 
-colors= np.array(['red', 'yellow', 'blue' ,'green'])
-plt.title('Pie Chart', fontweight='bold')
+x2 = np.array([0, 1, 2, 2, 3, 4, 5, 6, 7, 8, 8])
+y2 = np.array([50, 58, 65, 70, 72, 78, 83, 88, 92, 95, 97])
+
+styles = {
+    'fontweight': 'bold',
+    'fontsize' : 18,
+    }
+
+plt.xlabel('Hours Studied', **styles)
+plt.ylabel('Test Scores', **styles)
+plt.title('Scatter Plot', **styles)
 
 
-plt.pie(values, labels=categories,
-        autopct='%1.1f%%',
-        colors=colors,
-        explode=[0, 0, 0, 0.2],
-        shadow=True,
+plt.scatter(x1, y1,
+            marker='o',
+            s=150,
+            color='cyan',
+            alpha=0.5,
+            label='Class A'
 )
+plt.scatter(x2, y2,
+            marker='o',
+            s=150,
+            color='#9fe0d8',
+            alpha=0.5,
+            label='Class B'
+)
+plt.legend();
 plt.show()
