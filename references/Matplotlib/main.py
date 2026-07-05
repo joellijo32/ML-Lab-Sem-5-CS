319bd02a7a91777cc6d37361cefba18c4a64b7b2
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-05 15:06:04 +0530

    Plot labelling

--- references/Matplotlib/main.py
+++ references/Matplotlib/main.py
@@ -4,22 +4,27 @@ import numpy as np
 x = np.array([2023, 2024, 2025, 2026])
 y1 = np.array([23, 56, 3, 5])
 y2 = np.array([34, 7, 25, 8])
-y3 = np.array([2,4,6,25])
+y3 = np.array([2, 4, 6, 25])
 
-line_style = dict(
-marker='o',
-markerfacecolor='crimson',
-markeredgecolor='black',
-markersize=10,
-linestyle='solid',
-linewidth=3,
-)
+plt.title("Simple graph", 
+        fontsize='20',
+        family='Arial',
+        fontweight='bold',
+        color='blue')
+plt.xlabel('Year', fontsize=20,
+           family='Arial',
+           color='green',
+           fontweight='bold')
+plt.ylabel('Students', fontsize=20,
+           family='Arial',
+           color='green',
+           fontweight='bold')
+plt.xticks(x)
+plt.tick_params(axis='both', colors='magenta')
 
-plt.plot(x, y1, color='blue',  **line_style)
 
-plt.plot(x, y2, color='green', **line_style)
+plt.plot(x, y1, marker='*', markersize='8')
+plt.plot(x, y2, marker='*', markersize='8')
+plt.plot(x, y3, marker='*', markersize='8')
 
-plt.plot(x, y3, color='red', **line_style)
-
-plt.title("Simple graph")
 plt.show()
