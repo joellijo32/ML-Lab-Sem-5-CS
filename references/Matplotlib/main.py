3782137e4c2c329e76462712cb0a6498d47f7193
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-05 13:13:03 +0530

    Plot customization

--- references/Matplotlib/main.py
+++ references/Matplotlib/main.py
@@ -2,7 +2,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.array([2023, 2024, 2025, 2026])
-y = np.array([23, 56, 3, 5])
+y1 = np.array([23, 56, 3, 5])
+y2 = np.array([34, 7, 25, 8])
+y3 = np.array([2,4,6,25])
 
-plt.plot(x, y)
+line_style = dict(
+marker='o',
+markerfacecolor='crimson',
+markeredgecolor='black',
+markersize=10,
+linestyle='solid',
+linewidth=3,
+)
+
+plt.plot(x, y1, color='blue',  **line_style)
+
+plt.plot(x, y2, color='green', **line_style)
+
+plt.plot(x, y3, color='red', **line_style)
+
+plt.title("Simple graph")
 plt.show()
