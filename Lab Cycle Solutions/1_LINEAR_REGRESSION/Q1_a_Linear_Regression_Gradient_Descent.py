610a913ded77eec36601fe67d23a597bf0f8c388
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-08 21:23:53 +0530

    Q1 Gradient Descent R2 complete

--- Lab Cycle Solutions/1_LINEAR_REGRESSION/Q1_a_Linear_Regression_Gradient_Descent.py
+++ Lab Cycle Solutions/1_LINEAR_REGRESSION/Q1_a_Linear_Regression_Gradient_Descent.py
@@ -5,7 +5,7 @@ from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import SGDRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 
 california = fetch_california_housing()
 
@@ -35,9 +35,11 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
 mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
 
 print("\nGradient Descent\n")
 print("Intercept:", model.intercept_[0])
 print("Coefficient:", model.coef_[0])
 print("Mean Squared Error:", mse)
+print("R Squared (R²):", r2)
 
