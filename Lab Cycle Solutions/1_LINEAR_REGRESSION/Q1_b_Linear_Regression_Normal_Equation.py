16d6b7119c180788b67ee098ef3768c0424f5828
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-08 21:28:10 +0530

    Q1 Normal Equation R2 complete

--- Lab Cycle Solutions/1_LINEAR_REGRESSION/Q1_b_Linear_Regression_Normal_Equation.py
+++ Lab Cycle Solutions/1_LINEAR_REGRESSION/Q1_b_Linear_Regression_Normal_Equation.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 
 california = fetch_california_housing()
 
@@ -23,9 +23,12 @@ model = LinearRegression()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
+
 mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
 
 print('\nNormal Equation\n')
 print('Intercept:', model.intercept_)
 print('Coefficient:', model.coef_[0])
 print('Mean Squared Error:', mse)
+print('R Squared (R²):', r2)
