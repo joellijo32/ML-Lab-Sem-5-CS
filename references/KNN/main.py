c557e35994f8575c05307c6236893d7431249453
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-23 12:20:51 +0530

    knn ref: plotting of all three classes

--- references/KNN/main.py
+++ references/KNN/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
 
 iris = load_iris()
 df = pd.DataFrame(iris.data, columns=iris.feature_names)
@@ -10,5 +11,27 @@ df['target'] = iris.target
 df0 = df[df.target == 0]
 df1 = df[df.target == 1]
 df2 = df[100:]
+
+# Plotting all three classes
+
+x0 = df0['sepal length (cm)']
+y0 = df0['sepal width (cm)']
+
+x1 = df1['sepal length (cm)']
+y1 = df1['sepal width (cm)']
+
+x2 = df2['sepal width (cm)']
+y2 = df2['sepal width (cm)']
+
+plt.xlabel('sepal length (cm)')
+plt.ylabel('sepal width (cm)')
+plt.scatter(x0, y0, label='df0', alpha=0.7, s=100)
+plt.scatter(x1, y1, label='df1', alpha=0.7, s=100)
+plt.scatter(x2, y2, label='df2', alpha=0.7, s=100)
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+
 
 
