07e2506ad31b0e9613b272929797b13a48414f8a
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-23 11:10:52 +0530

    Q8 code cleanup

--- Lab Cycle Solutions/8_KNN/Q8_KNN.py
+++ Lab Cycle Solutions/8_KNN/Q8_KNN.py
@@ -10,27 +10,16 @@ import time
 
 x_train=x_train/255.0
 x_test=x_test/255.0
-#x_train.shape
-#x_test.shape
-#len(x_train)
 
 #convert into 1 dimension
 x_train=x_train.reshape(len(x_train),-1)
 x_test=x_test.reshape(len(x_test),-1)
-#y_train=y_train.reshape(len(y_train),-1)
-#x_train.shape
 
 #take only the first 5000data
 x_train=x_train[:5000]
 y_train=y_train[:5000]
 x_test=x_test[:1000]
 y_test=y_test[:1000]
-
-"""
-def euclideanDis(x1, x2):
-  return np.sqrt(np.sum((x1 - x2)**2))
-"""
-
 
 for i in range(3,10):
   start_time=time.time()
@@ -42,5 +31,4 @@ for i in range(3,10):
   print(f"K value :{i}")
   print(f"Accuracy:{accuracy_score(y_test,Y)}")
   end_time=time.time()
-  print(f"Time:{end_time-start_time}")
-  print()
+  print(f"Time:{end_time-start_time}\n")
