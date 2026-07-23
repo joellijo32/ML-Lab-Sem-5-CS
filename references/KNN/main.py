28fd23bc185bb11a371b50336a13fc4664e89a8b
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-23 12:49:52 +0530

    knn ref: model prediction and confusion matrix

--- references/KNN/main.py
+++ references/KNN/main.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
 
 iris = load_iris()
 df = pd.DataFrame(iris.data, columns=iris.feature_names)
@@ -39,3 +41,15 @@ plt.show()
 x = df.drop(['target'], axis='columns')
 y = df.target
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+# Model training and prediction
+
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(X_train, y_train)
+
+score = knn.score(X_test, y_test)
+print("Prediction score:", score)
+
+y_pred = knn.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
