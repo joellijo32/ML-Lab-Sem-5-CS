9b7802675679d419d5c17eda8faa876d19025fd3
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-28 21:34:21 +0530

    lin reg ref: data target add

--- references/LinearRegression/GradientDescent/main.py
+++ references/LinearRegression/GradientDescent/main.py
@@ -3,5 +3,7 @@ from sklearn.datasets import fetch_california_housing
 import pandas as pd
 
 data = fetch_california_housing()
-df = pd.DataFrame(data, columns=data.feature_names)
-print(df)
+df = pd.DataFrame(data.data, columns=data.feature_names)
+df['MedHouseVal'] = data.target
+
+print(df.head())
