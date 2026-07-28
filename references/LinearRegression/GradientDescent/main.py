9ab84114c0f88dabeece66641e0a62a11c03ea8a
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-28 21:42:51 +0530

    lin reg ref: lin reg model import

--- references/LinearRegression/GradientDescent/main.py
+++ references/LinearRegression/GradientDescent/main.py
@@ -1,9 +1,14 @@
 import numpy as np
 from sklearn.datasets import fetch_california_housing
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 data = fetch_california_housing()
 df = pd.DataFrame(data.data, columns=data.feature_names)
 df['MedHouseVal'] = data.target
 
-print(df.head())
+X = df['AveRooms'][:5000]
+y = df['MedHouseVal'][:5000]
+
+model = LinearRegression()
+
