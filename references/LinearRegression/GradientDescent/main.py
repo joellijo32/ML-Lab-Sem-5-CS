f83b2fd446ac3d5597e73ebd68880ebf1d0c7890
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-28 22:11:55 +0530

    lin reg ref: scatter plot

--- references/LinearRegression/GradientDescent/main.py
+++ references/LinearRegression/GradientDescent/main.py
@@ -3,19 +3,29 @@ from sklearn.datasets import fetch_california_housing
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
 data = fetch_california_housing()
 df = pd.DataFrame(data.data, columns=data.feature_names)
 df['MedHouseVal'] = data.target
 
-X = np.array(df['AveRooms'][:5000])
-y = np.array(df['MedHouseVal'][:5000])
+X = np.array(df['AveRooms'][:1000])
+y = np.array(df['MedHouseVal'][:1000])
 X = X.reshape(-1, 1)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-# print(y_train)
+y_pred = model.predict(X_test)
+
+plt.scatter(X_test, y_test)
+plt.show()
+
 
