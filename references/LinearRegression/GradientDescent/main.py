4d30a8a2ff6d05a88402cebe066fb6bcee7cae2b
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-28 21:56:45 +0530

    lin reg ref: model fitting

--- references/LinearRegression/GradientDescent/main.py
+++ references/LinearRegression/GradientDescent/main.py
@@ -2,13 +2,20 @@ import numpy as np
 from sklearn.datasets import fetch_california_housing
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 data = fetch_california_housing()
 df = pd.DataFrame(data.data, columns=data.feature_names)
 df['MedHouseVal'] = data.target
 
-X = df['AveRooms'][:5000]
-y = df['MedHouseVal'][:5000]
+X = np.array(df['AveRooms'][:5000])
+y = np.array(df['MedHouseVal'][:5000])
+X = X.reshape(-1, 1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 model = LinearRegression()
+model.fit(X_train, y_train)
+
+# print(y_train)
 
