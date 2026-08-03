8b5e0e35e1bd60f5cec5821b8a6f38323c9bbd9a
Joel Lijo Mathew <joellijo4@gmail.com>
2026-08-03 22:24:13 +0530

    Q1 b refining

--- Lab Cycle Solutions/1_LINEAR_REGRESSION/Q1_b_Linear_Regression_Normal_Equation.py
+++ Lab Cycle Solutions/1_LINEAR_REGRESSION/Q1_b_Linear_Regression_Normal_Equation.py
@@ -5,24 +5,26 @@ from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler
 
 california = fetch_california_housing()
-
 df = pd.DataFrame(california.data, columns=california.feature_names)
 df['MedHouseVal'] = california.target
 
-X = df[['AveRooms']]
-y = df['MedHouseVal']
+X = df[['AveRooms']][:1000]
+y = df['MedHouseVal'][:1000]
 
 X_train, X_test, y_train, y_test = train_test_split(
 	X, y, test_size=0.2
 )
 
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
 model = LinearRegression()
-
-model.fit(X_train, y_train)
-
-y_pred = model.predict(X_test)
+model.fit(X_train_scaled, y_train)
+y_pred = model.predict(X_test_scaled)
 
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
