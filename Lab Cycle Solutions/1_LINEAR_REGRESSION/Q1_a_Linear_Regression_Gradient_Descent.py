87a5f02624a08045f38e42f17fb3de2cffc29e5c
Joel Lijo Mathew <joellijo4@gmail.com>
2026-08-03 22:21:19 +0530

    Q1 refining

--- Lab Cycle Solutions/1_LINEAR_REGRESSION/Q1_a_Linear_Regression_Gradient_Descent.py
+++ Lab Cycle Solutions/1_LINEAR_REGRESSION/Q1_a_Linear_Regression_Gradient_Descent.py
@@ -8,31 +8,29 @@ from sklearn.linear_model import SGDRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
 california = fetch_california_housing()
-
 df = pd.DataFrame(california.data, columns=california.feature_names)
 df['MedHouseVal'] = california.target
 
-X = df[['AveRooms']]
-y = df['MedHouseVal']
+X = df[['AveRooms']][:1000]
+y = df['MedHouseVal'][:1000]
 
 X_train, X_test, y_train, y_test = train_test_split(
 	X, y, test_size=0.2
 )
 
 scaler = StandardScaler()
-
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
 
 model = SGDRegressor(
 	max_iter=1000,
 	learning_rate='constant',
 	eta0=0.01,
+	penalty = None
 )
 
-model.fit(X_train, y_train)
-
-y_pred = model.predict(X_test)
+model.fit(X_train_scaled, y_train)
+y_pred = model.predict(X_test_scaled)
 
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
