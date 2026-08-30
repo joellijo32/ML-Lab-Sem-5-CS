8d7387d35411b52684b1271b4482af40286170e0
Joel Lijo Mathew <joellijo4@gmail.com>
2026-08-30 16:41:04 +0530

    Q9 algo implementation

--- Lab Cycle Solutions/9_ID3_DecisionTree/Q9_ID3.py
+++ Lab Cycle Solutions/9_ID3_DecisionTree/Q9_ID3.py
@@ -4,11 +4,13 @@ from ucimlrepo import fetch_ucirepo
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.metrics import accuracy_score
+import pandas as pd
 
 online_retail = fetch_ucirepo(id=352) # id of online retail dataset= 352
+df = online_retail.data.features
 
 df = df.dropna(subset=["CustomerID"])
-df = df[df["Quantity"] >= 0]
+df = df[(df["Quantity"] > 0) & (df["UnitPrice"] > 0)]
 df["TotalAmount"] = df["Quantity"] * df["UnitPrice"]
 
 customer = df.groupby("CustomerID").add({
@@ -18,4 +20,26 @@ customer = df.groupby("CustomerID").add({
 	"TotalAmount" : "sum"
 })
 
+median = customer["TotalAmount"].median()
 
+customer["Segment"] = (customer["TotalAmount"] >= median).astype(int)
+
+X = customer[["Quantity", "UnitPrice", "InvoiceNo"]]
+y = customer["Segment"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+model = DecisionTreeClassifier(criterion = "entropy", random_state = 42)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print(f"Accuracy = {accuracy}")
+
+print("\nFeature Importance: ")
+
+for feature, importance in zip(X.columns, model.feature_importances_):
+	print(f"{feature} : {importance:.4f}")
